1bc85515791e67ca7140c47f33997734efbeacbf
arefhbb <haref3460@gmail.com>
2022-04-18 01:30:12 +0430

    Some server side features are complete

--- server/server.py
+++ server/server.py
@@ -2,7 +2,6 @@ import socket
 import random
 import os
 
-
 # def dwld(path):
 #     randomPort = random.randint(3000,50000)
 #     dwldSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -24,20 +23,51 @@ import os
 #     dwldSocket.close()
 
 
-appSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+def Help():
+    return "help -> Display a list of commands\nlist -> List of files and their size in a directory\ndwld -> Download files\npwd -> Our current location in a directory\ncd -> Change directory"
 
-appSocket.bind(("127.0.0.1",2121))
-appSocket.listen(5)
 
-#listen for connection
-connection,client = appSocket.accept()
-print('connexted: ',client)
+def List():
+    sizeAllFile = 0
+    arr = []
+    ldr = os.listdir(Pwd())
+    for i in ldr:
+        p = []
+        size = os.stat(i).st_size
+        p.append(i)
+        p.append(os.path.isdir(i))
+        p.append(size)
+        arr.append(p)
+        sizeAllFile += size
+    arr.append(sizeAllFile)
+    return arr
+
+
+def Pwd():
+    return os.getcwd()
+
+
+appSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+appSocket.bind(("127.0.0.1", 2121))
+appSocket.listen(1)
+
+# listen for connection
+connection, client = appSocket.accept()
+
+print(client, "connected")
 
 while True:
-    msg = connection.recv(1024).decode()
-    command,path = msg.split(' ')
-    command=command.lower()
-    print("Recieved: ",msg)
+    data = connection.recv(64).decode().split(" ")
+    data[0] = data[0].lower()
+    print("Recieved: ", data)
 
-appSocket.close()
+    if data[0] == "!":
+        break
+    elif data[0] == "help":
+        Help()
+    elif data[0] == "list":
+        List()
+    elif data[0] == "pwd":
+        Pwd()
 
+connection.close()
